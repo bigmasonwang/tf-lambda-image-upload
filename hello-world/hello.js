d5d6c80d1ad6a6611a094a9f9bf404f4aa797a3e
mason wang <38519942+bigmasonwang@users.noreply.github.com>
2021-11-21 21:48:48 +1030

    Update hello.js

--- hello-world/hello.js
+++ hello-world/hello.js
@@ -83,6 +83,7 @@ module.exports.handler = async (event) => {
     statusCode: 200,
     headers: {
       'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
     },
     body: JSON.stringify({
       tempUrl,
